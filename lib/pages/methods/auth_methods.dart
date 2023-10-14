035dfcef6f6d2d649da7c2846c5e76b09f6cc967
Emmanuel Bett <emmanuelkbett3@gmail.com>
2023-10-14 13:02:18 +0300

    login

---
+++ lib/pages/methods/auth_methods.dart
@@ -0,0 +1,10 @@
+class AuthMethods{
+  Future<String> login(String email, String password)async{
+    return "";
+  }
+  Future<String> signup(String email, String password)async{
+      return "";
+  }
+
+  
+}
